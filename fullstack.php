7ed76adbeca06e3ecedc8839a794d25a78a18c2b
Steve Nyanumba <reach@stevennmb9@gmail.com>
2025-01-31 10:16:15 +0300

    Starting Fullstack development

---
+++ fullstack.php
@@ -0,0 +1,164 @@
+// Languages We will be dealing with:
+
+// 1. HTML
+// 2. CSS
+// 3. JavaScript
+// 4. PHP
+// 5. MySQL
+// 6. Node.js
+
+
+//Frontend
+// Frontend is the part of the website that the user sees and interacts with.
+
+// Backend
+// Backend is the part of the website that the user does not see and interacts with.
+// It involves the server side of the website. 
+
+// Fullstack
+// Fullstack is the combination of frontend and backend.
+// It involves the server side of the website as well as the client side of the website.
+
+//Database
+// Database is a collection of data that is stored in a structured or unstructed format.
+
+// Structured Databases     
+    // 1. MySQL
+    // 2. PostgreSQL
+    // 3. SQLite
+    // 4. Oracle
+    // 5. Microsoft SQL Server
+
+// Unstructured Databases (Rarely used)
+    // 1. MongoDB
+    // 2. CouchDB
+    // 3. Cassandra
+    // 4. Redis
+    // 5. Neo4j
+
+
+// Preprocessors for Frontend and Backend
+// They are used to preprocess the data before it is sent to their respective servers or clients.
+
+//CSS Preprocessors
+    // 1. SASS (Most Popular)
+    // 2. LESS
+    // 3. Stylus
+
+//JavaScript Preprocessors
+    // 1. TypeScript (Most Popular)
+    // 2. CoffeeScript
+    // 3. Babel
+
+//HTML Preprocessors
+    // 1. Pug (Most Popular)
+    // 2. EJS
+    // 3. Handlebars
+
+//PHP Preprocessors
+    // 1. Blade (Most Popular)
+    // 2. Twig
+    // 3. Smarty
+
+
+
+    //The thing about preprocessors is they are not necessary for the website to work.
+    // They are just used to make the code more readable and easier to write.
+    // They are also used to make the code more efficient.
+    // They are also used to make the code more secure.
+
+// preprocessors need an external compiler to compile the code into CSS or JavaScript.
+
+    //Compilers
+    // 1. Vite
+    // 2. Webpack
+    // 3. Gulp  
+
+// Vite is the most popular compiler.
+// Webpack is the second most popular compiler.
+// Gulp is the third most popular compiler.
+
+
+// Frameworks
+// Frameworks are libraries that are used to build the website.
+
+// Libraries are collections of code that are used to build the website.
+
+// Frameworks are more powerful than libraries.
+
+//Frontend Frameworks
+
+// Javascript Frameworks
+    // 1. React (Most Popular)
+    // 2. Vue
+    // 3. Angular
+
+// CSS Frameworks
+    // 1. Bootstrap (Most Popular)
+    // 2. Tailwind
+    // 3. Foundation
+    // 4. Bulma
+    // 5. Semantic UI
+    // 6. Materialize
+    // 7. UIKit
+    // 8. Skeleton
+    // 9. Pure
+    // 10. UI
+
+//Backend Frameworks
+
+//PHP Frameworks
+    // 1. Laravel (Most Popular)
+    // 2. CodeIgniter
+    // 3. Symfony
+    // 4. Yii
+    // 5. CakePHP
+    // 6. Phalcon
+    // 7. Zend
+    // 8. Slim
+    // 9. FuelPHP
+    // 10. Yii2
+
+//Python Frameworks
+    // 1. Django (Most Popular)
+    // 2. Flask
+    // 3. Pyramid
+    // 4. FastAPI
+    // 5. Tornado
+    // 6. CherryPy
+    // 7. Bottle
+    // 8. Sanic
+    // 9. Hug
+    // 10. Falcon
+
+// Mobile Frameworks
+    // 1. React Native (Most Popular)
+    // 2. Flutter
+    // 3. Ionic
+    // 4. NativeScript
+    // 5. Xamarin
+    // 6. Cordova
+    // 7. PhoneGap
+    // 8. Appcelerator
+    // 9. Telerik
+    // 10. Intel XDK
+
+// Game Development Frameworks
+    // 1. Unity (Most Popular)
+    // 2. Unreal Engine
+    // 3. Godot
+    // 4. GameMaker Studio
+    // 5. Construct
+    // 6. GameSalad
+
+// AI Frameworks
+    // 1. OpenAI (Most Popular)
+    // 2. Google AI - Gemini
+    // 3. Microsoft AI - Azure AI
+    // 4. IBM AI
+    // 5. Amazon AI - Amazon Bedrock
+
+
+
+
+                        // LET'S GET STARTED WITH LARAVEL
